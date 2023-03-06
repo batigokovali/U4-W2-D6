dbc331cea7c7234430b48e03eb49c2d9a49b9026
Batı Gökovalı <batigokovali@icloud.com>
2023-03-06 18:22:52 +0300

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,34 +3,13 @@ import { Container, Button } from "react-bootstrap";
 
 const Footer = (props) => {
   const getAuthors = async () => {
-    await fetch("http://localhost:3001/authors")
+    await fetch("https://u4-w1-d4-production.up.railway.app/authors")
       .then(response => response.json())
       .then(data => console.log("authors:", data));
   }
 
-  const getSpecificAuthors = async () => {
-    await fetch("http://localhost:3001/authors/dd82z1hfkleoccco4")
-      .then(response => response.json())
-      .then(data => console.log("Sample author:", data));
-  }
-
-  const getBlogposts = async () => {
-    await fetch("http://localhost:3001/blogposts")
-      .then(response => response.json())
-      .then(data => console.log("Blogposts:", data));
-  }
-
-  const getAllCommentsFromSpecificBlogpost = async () => {
-    await fetch("http://localhost:3001/blogposts")
-      .then(response => response.json())
-      .then(data => console.log("Blogposts:", data));
-  }
-
   useEffect(() => {
     getAuthors()
-    getBlogposts()
-    getSpecificAuthors()
-    getAllCommentsFromSpecificBlogpost()
   }, [])
 
   return (
