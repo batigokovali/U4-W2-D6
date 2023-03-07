42460c51a32570830560c13918c954522e2082ef
Batı Gökovalı <batigokovali@icloud.com>
2023-03-07 19:11:16 +0300

    update

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row, Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const BlogList = (props) => {
 
@@ -26,18 +27,22 @@ const BlogList = (props) => {
     <Row>
       {blogpost && blogpost.map((post) => {
         return (
+
           <Col className="border p-3" xs={3} md={3} lg={3}>
-            <Card.Img variant="top" src={post.coverURL} className="blog-cover" />
-            <Card.Body>
-              <Card.Title>Title: {post.title}</Card.Title>
-            </Card.Body>
-            <Card.Footer>
-              <Card.Title>Author: {post.author.name} </Card.Title>
-            </Card.Footer>
-            <a onClick={e => e.stopPropagation} href={`${process.env.REACT_APP_BE_URL}/blogposts/${post.id}/pdf`}>
-              Download as pdf!
-            </a>
-          </Col>)
+            <Link to={`/blog/${post.id}`}>
+              <Card.Img variant="top" src={post.coverURL} className="blog-cover" />
+              <Card.Body>
+                <Card.Title>Title: {post.title}</Card.Title>
+              </Card.Body>
+              <Card.Footer>
+                <Card.Title>Author: {post.author.name} </Card.Title>
+              </Card.Footer>
+              <a onClick={e => e.stopPropagation} href={`${process.env.REACT_APP_BE_URL}/blogposts/${post.id}/pdf`}>
+                Download as pdf!
+              </a>
+            </Link>
+          </Col>
+        )
       })}
     </Row>
   );
